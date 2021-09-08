5a3c12d33dc238c009f535d89176824cc856d6c5
Jimmychao49 <49746073+Jimmychao49@users.noreply.github.com>
2021-09-08 22:56:17 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+var x = 50;
+var y = 50;
+var speedY = Math.floor((Math.random() * 10) + 1);
+var speedX = Math.floor((Math.random() * 10) + 1);
+
 /**
  * setup
  * de code in deze functie wordt één keer uitgevoerd door
@@ -8,7 +13,7 @@ function setup() {
   createCanvas(1280, 720);
 
   // Kleur de achtergrond blauw, zodat je het kunt zien
-  background('blue');
+  background('black');
 }
 
 
@@ -18,9 +23,22 @@ function setup() {
  * uitgevoerd door de p5 library, nadat de setup functie klaar is
  */
 function draw() {
+
+  background('black');
   // stel vulkleur in
   fill(100, 100, 255);
 
   // teken een cirkel
-  ellipse(50,50,80,80);
+  ellipse(x,y,80,80);
+
+  x = x + speedX;
+  y = y + speedY;
+
+  if (y > 680 || y < 40) {
+    speedY = speedY * -1;
+  }
+
+  if (x > 1240 || x < 40) {
+    speedX = speedX * -1;
+  }
 }
